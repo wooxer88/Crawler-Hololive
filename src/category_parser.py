ae639c1e119eb7052fdfb6d44b84b04e1dc4c9b9
ChinWei Shiu <wooxer88@gmail.com>
2022-01-09 19:15:13 +0800

    Add Hololive category & data checksum

---
+++ src/category_parser.py
@@ -0,0 +1,30 @@
+import hashlib
+import json
+import os
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+from utils import get_project_root, update_checksum
+
+def main():
+  file_path = os.path.join(get_project_root(), 'data', 'category.json')
+  url = 'https://hololive.hololivepro.com/talents'
+  res = requests.get(url)
+  data = []
+
+  Soup = BeautifulSoup(res.text, 'html.parser')
+  Soup_res = Soup.find(id='nav_tag').findAll('a')
+
+  for index, item in enumerate(Soup_res):
+    data.append({ 'label': item.get_text(), 'url': item.get('href') })
+
+  check_code = hashlib.sha256(json.dumps(data).encode('utf-8')).hexdigest()
+
+  with open(file_path, 'w', encoding='utf-8') as f:
+    json.dump({ 'data': data }, f, indent=2, ensure_ascii=False)
+
+  update_checksum('category', check_code)
+
+if __name__ == '__main__':
+  sys.exit(main())
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import json
+import os
+import time
+
+from datetime import datetime
+from pathlib import Path
+
+def get_project_root() -> Path:
+  return Path(__file__).parent.parent
+
+def update_checksum(key, code):
+  file_path = os.path.join(get_project_root(), 'data-checksum.json')
+  timestamp = round(datetime.now().timestamp())
+  
+  with open(file_path) as f:
+    data = json.load(f)
+
+  data[key]['code'] = code
+  data[key]['timestamp'] = timestamp
+
+  with open(file_path, 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=2, ensure_ascii=False)
+
