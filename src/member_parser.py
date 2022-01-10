cd1be346c26978f7ca71a2c31891f41bf722fe9c
ChinWei Shiu <wooxer88@gmail.com>
2022-01-10 20:48:50 +0800

    Fix overseas member name & AZKi name

--- src/member_parser.py
+++ src/member_parser.py
@@ -9,16 +9,43 @@ from bs4 import BeautifulSoup
 from typing import List
 from utils import get_project_root, update_checksum
 
+en_label_data = ['ホロライブEnglish', 'Myth', 'Project: HOPE', 'Council']
+id_label_data = ['ホロライブインドネシア']
+
 def main():
-  category_url = 'https://raw.githubusercontent.com/wooxer88/Crawler-Hololive/ae639c1e119eb7052fdfb6d44b84b04e1dc4c9b9/data/category.json'
+  category_url = 'https://raw.githubusercontent.com/wooxer88/Crawler-Hololive/main/data/category_min.json'
+  # detail_url = 'https://raw.githubusercontent.com/wooxer88/Crawler-Hololive/main/data/member_detail_min.json'
   category_res = requests.get(category_url)
   category_json = json.loads(category_res.text)['data']
+  # detail_res = requests.get(detail_url)
+  # detail_json = json.loads(detail_res.text)['data']
   file_path = os.path.join(get_project_root(), 'data', 'member.json')
   file_min_path = os.path.join(get_project_root(), 'data', 'member_min.json')
   member_data = {}
+  overseas_data = {}
 
   for item in category_json:
     member_data[item['label']] = category_member(item['label'], item['url'])
+
+    if item['label'] in en_label_data or item['label'] in id_label_data:
+      for item2 in member_data[item['label']]:
+        overseas_data[item2['jp_name']] = item2['name']
+  # fix member en and id name in ALL of category
+  for item in member_data['ALL']:
+    if item['name'] in overseas_data:
+      item['name'] = overseas_data[item['name']]
+
+  # for item in member_data:
+  #   for item2 in member_data[item]:
+  #     url = item2['url']
+  #     detail = detail_json[item2['name']]
+
+  #     for item3 in detail: item2[item3] = detail[item3]
+
+  #     item2['avatar']['hololive'] = item2['img']
+  #     item2['url']['hololive'] = url
+
+  #     del item2['img']
 
   check_code = hashlib.sha256(json.dumps(member_data).encode('utf-8')).hexdigest()
 
@@ -30,8 +57,6 @@ def main():
   update_checksum('member', check_code)
 
 def category_member(category, url) -> List:
-  en_label_data = ['ホロライブEnglish', 'Myth', 'Project: HOPE', 'Council']
-  id_label_data = ['ホロライブインドネシア']
   res = requests.get(url)
   data = []
 
@@ -48,6 +73,10 @@ def category_member(category, url) -> List:
       name = roman_name
     else:
       name = jp_name
+
+      if name == 'AZKi':
+        jp_name = ''
+        roman_name = name
 
     data.append({
       'img': item.find('img').get('src'),
